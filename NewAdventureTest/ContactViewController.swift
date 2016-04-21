5b03fb31721fd37014b4ca6ebd77bd11cf2f219e
DFeredinos <DFeredinos@gmail.com>
2016-04-21 14:56:23 -0700

    First Commit
    iOS Test

---
+++ NewAdventureTest/ContactViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ContactViewController.swift
+//  NewAdventureTest
+//
+//  Created by Demitri Feredinos on 4/20/16.
+//  Copyright © 2016 Demitri Feredinos. All rights reserved.
+//
+
+import UIKit
+
+class ContactViewController: UIViewController {
+    @IBOutlet weak var scrollView: UIScrollView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.addSubview(scrollView)
+
+        // Do any additional setup after loading the view.
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        
+        scrollView.contentSize = CGSizeMake(375, 800)
+    }
+   
+}
---
+++ NewAdventureTest/productViewController.swift
@@ -0,0 +1,28 @@
+//
+//  productViewController.swift
+//  NewAdventureTest
+//
+//  Created by Demitri Feredinos on 4/20/16.
+//  Copyright © 2016 Demitri Feredinos. All rights reserved.
+//
+
+import UIKit
+
+class productViewController: UIViewController {
+
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productImageView: UIImageView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        productNameLabel.text = "A New Adventure"
+        productImageView.image = UIImage(named: "IMG_4131")
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func clickForFunPressed(sender: AnyObject) {
+        print("I'm a hottie baby")
+    }
+}
